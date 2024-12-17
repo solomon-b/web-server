b41f54ed6f1fe9b4796f960815730c7cd3ae538f
solomon <ssbothwell@gmail.com>
2024-12-17 15:02:10 -0800

    Fix escaping in Blog Preview template.

--- server/src/Utils.hs
+++ server/src/Utils.hs
@@ -6,6 +6,9 @@ module Utils where
 import Data.Aeson (FromJSON)
 import Data.Aeson qualified as Aeson
 import Data.ByteString.Lazy qualified as BL
+import Data.Text (Text)
+import Data.Text qualified as Text
+import Network.URI qualified as URI
 
 --------------------------------------------------------------------------------
 
@@ -19,3 +22,9 @@ readJSON path' = do
 
 combine :: (Applicative f) => (f a, f b) -> f (a, b)
 combine (x, y) = liftA2 (,) x y
+
+escapeString :: Text -> Text
+escapeString = Text.pack . URI.escapeURIString URI.isUnreserved . Text.unpack
+
+unEscapeString :: Text -> Text
+unEscapeString = Text.pack . URI.unEscapeString . Text.unpack
