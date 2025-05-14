a31398e9b09e26d8158a3bcb76cffcfda0816f47
solomon <ssbothwell@gmail.com>
2025-05-13 20:33:18 -0700

    Updates hsql-pool acquisition in web-server-core.

--- web-server-core/src/App.hs
+++ web-server-core/src/App.hs
@@ -33,13 +33,15 @@ import Data.Aeson.KeyMap qualified as KeyMap
 import Data.Bifunctor (bimap)
 import Data.CaseInsensitive qualified as CI
 import Data.Data (Proxy (..))
-import Data.Foldable (fold)
 import Data.Function ((&))
 import Data.Maybe (catMaybes, fromMaybe)
 import Data.Text.Display (display)
+import Data.Text.Encoding qualified as TE
 import Data.Text.Encoding qualified as Text.Encoding
 import Data.Time (getCurrentTime)
-import Hasql.Connection qualified as HSQL
+import Hasql.Connection.Setting qualified as HSQL.Setting
+import Hasql.Connection.Setting.Connection qualified as HSQL.Connection
+import Hasql.Connection.Setting.Connection.Param qualified as HSQL.Params
 import Hasql.Pool qualified as HSQL.Pool
 import Hasql.Pool.Config as HSQL.Pool.Config
 import Log qualified
@@ -76,11 +78,8 @@ runApp server ctx = do
             logEnv = Log.LoggerEnv stdOutLogger "webserver-backend" [] [] maxLogLevel
         Log.logMessageIO logEnv time Log.LogInfo "Launching Service" (Aeson.toJSON $ KeyMap.fromList ["port" .= warpConfigPort appConfigWarpSettings, "environment" .= appConfigEnvironment, "log-level" .= maxLogLevel])
 
-        -- DB Pool
-        let PostgresConfig {..} = appConfigPostgresSettings
-        let hsqlSettings = HSQL.settings (fold postgresConfigHost) (fromMaybe 0 postgresConfigPort) (fold postgresConfigUser) (fold postgresConfigPassword) (fold postgresConfigDB)
-        let poolSettings = HSQL.Pool.Config.settings [HSQL.Pool.Config.staticConnectionSettings hsqlSettings]
-        pgPool <- HSQL.Pool.acquire poolSettings
+        Log.logMessageIO logEnv time Log.LogInfo "Acquiring Postgres Connection Pool" (Aeson.object [])
+        pgPool <- acquirePool appConfigPostgresSettings
 
         -- DB Healthcheck
         healthCheckResult <- flip runReaderT pgPool $ execStatement healthCheck
@@ -96,6 +95,19 @@ runApp server ctx = do
               appContext = AppContext pgPool tracer hostname appConfigEnvironment logEnv ctx
               warpSettings = mkWarpSettings logEnv appConfigWarpSettings
           Warp.runSettings warpSettings (otelMiddleware $ mkApp @api server servantContext appContext)
+
+acquirePool :: PostgresConfig -> IO HSQL.Pool.Pool
+acquirePool PostgresConfig {..} = do
+  let hsqlSettings :: Maybe [HSQL.Setting.Setting]
+      hsqlSettings = do
+        host <- HSQL.Params.host . TE.decodeUtf8 <$> postgresConfigHost
+        port <- HSQL.Params.port <$> postgresConfigPort
+        user <- HSQL.Params.user . TE.decodeUtf8 <$> postgresConfigUser
+        password <- HSQL.Params.password . TE.decodeUtf8 <$> postgresConfigPassword
+        db <- HSQL.Params.dbname . TE.decodeUtf8 <$> postgresConfigDB
+        pure [HSQL.Setting.connection $ HSQL.Connection.params [host, port, user, password, db]]
+  let poolSettings = HSQL.Pool.Config.settings $ pure $ staticConnectionSettings $ fromMaybe [] hsqlSettings
+  HSQL.Pool.acquire poolSettings
 
 mkLogLevel :: Verbosity -> Log.LogLevel
 mkLogLevel = \case
