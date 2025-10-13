3da1f276892d31ae181a3f93af0b6108e0e9171d
solomon <ssbothwell@gmail.com>
2025-10-13 10:08:35 -0700

    chore: updates flake lock.

--- overlays/server.nix
+++ overlays/server.nix
@@ -5,25 +5,19 @@ final: prev: {
     overrides = hfinal: hprev: {
       hasql = final.haskell.lib.dontCheck final.haskellPackages.hasql_1_9_1_2;
 
-      hasql-pool = final.haskell.lib.dontCheck (
-        final.haskell.lib.dontCheck (
-          hfinal.callHackageDirect {
-            pkg = "hasql-pool";
-            ver = "1.3.0.2";
-            sha256 = "sha256-3tADBDSR7MErgVLzIZdivVqyU99/A7jsRV3qUS7wWns=";
-          } { }
-        )
-      );
+      hasql-pool = final.haskell.lib.dontCheck final.haskellPackages.hasql-pool_1_3_0_1;
 
-      hasql-transaction = final.haskell.lib.dontCheck (
-        final.haskell.lib.dontCheck (
-          hfinal.callHackageDirect {
-            pkg = "hasql-transaction";
-            ver = "1.2.0.1";
-            sha256 = "sha256-gXLDMlD6E3degEUJOtFCiZf9EAsWEBJqsOfZK54iBSA=";
-          } { }
-        )
-      );
+      # hasql-pool = final.haskell.lib.dontCheck (
+      #   final.haskell.lib.dontCheck (
+      #     hfinal.callHackageDirect {
+      #       pkg = "hasql-pool";
+      #       ver = "1.3.0.2";
+      #       sha256 = "sha256-3tADBDSR7MErgVLzIZdivVqyU99/A7jsRV3qUS7wWns=";
+      #     } { }
+      #   )
+      # );
+
+      hasql-transaction = final.haskell.lib.dontCheck final.haskellPackages.hasql-transaction_1_2_0_1;
 
       hasql-interpolate = final.haskell.lib.dontCheck (
         hfinal.callCabal2nix "hasql-interpolate" "${inputs.hasql-interpolate-src}" { }
--- overlays/web-server-core.nix
+++ overlays/web-server-core.nix
@@ -2,26 +2,18 @@ self: final: prev: {
   haskellPackages = prev.haskellPackages.override {
     overrides = hfinal: hprev: {
       hasql = final.haskell.lib.dontCheck final.haskellPackages.hasql_1_9_1_2;
+      hasql-pool = final.haskell.lib.dontCheck final.haskellPackages.hasql-pool_1_3_0_1;
+      hasql-transaction = final.haskell.lib.dontCheck final.haskellPackages.hasql-transaction_1_2_0_1;
 
-      hasql-pool = final.haskell.lib.dontCheck (
-        final.haskell.lib.dontCheck (
-          hfinal.callHackageDirect {
-            pkg = "hasql-pool";
-            ver = "1.3.0.2";
-            sha256 = "sha256-3tADBDSR7MErgVLzIZdivVqyU99/A7jsRV3qUS7wWns=";
-          } { }
-        )
-      );
-
-      hasql-transaction = final.haskell.lib.dontCheck (
-        final.haskell.lib.dontCheck (
-          hfinal.callHackageDirect {
-            pkg = "hasql-transaction";
-            ver = "1.2.0.1";
-            sha256 = "sha256-gXLDMlD6E3degEUJOtFCiZf9EAsWEBJqsOfZK54iBSA=";
-          } { }
-        )
-      );
+      # hasql-pool = final.haskell.lib.dontCheck (
+      #   final.haskell.lib.dontCheck (
+      #     hfinal.callHackageDirect {
+      #       pkg = "hasql-pool";
+      #       ver = "1.3.0.2";
+      #       sha256 = "sha256-3tADBDSR7MErgVLzIZdivVqyU99/A7jsRV3qUS7wWns=";
+      #     } { }
+      #   )
+      # );
 
       web-server-core = final.haskell.lib.dontCheck (
         hfinal.callCabal2nix "web-server-core" "${self}/web-server-core" { }
