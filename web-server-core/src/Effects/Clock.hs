b6965a317fc86884ac468dc49c2446e79a3381ca
solomon <ssbothwell@gmail.com>
2026-02-16 18:54:15 -0800

    feat: Adds ExceptT instances.

--- web-server-core/src/Effects/Clock.hs
+++ web-server-core/src/Effects/Clock.hs
@@ -8,6 +8,8 @@ where
 
 --------------------------------------------------------------------------------
 
+import Control.Monad.Trans (lift)
+import Control.Monad.Trans.Except (ExceptT)
 import Data.Time (UTCTime)
 import Data.Time.Clock qualified as Clock
 
@@ -15,3 +17,6 @@ import Data.Time.Clock qualified as Clock
 
 class MonadClock m where
   currentSystemTime :: m UTCTime
+
+instance (MonadClock m, Monad m) => MonadClock (ExceptT e m) where
+  currentSystemTime = lift currentSystemTime
--- web-server-core/src/Effects/Database/Class.hs
+++ web-server-core/src/Effects/Database/Class.hs
@@ -7,6 +7,8 @@ module Effects.Database.Class where
 import Control.Monad.IO.Class (MonadIO (..))
 import Control.Monad.Reader (ReaderT)
 import Control.Monad.Reader qualified as Reader
+import Control.Monad.Trans (lift)
+import Control.Monad.Trans.Except (ExceptT)
 import Data.Has (Has)
 import Data.Has qualified as Has
 import Hasql.Decoders ()
@@ -17,8 +19,8 @@ import Hasql.Pool qualified as HSQL.Pool
 import Hasql.Session (Session)
 import Hasql.Session qualified as Hasql
 import Hasql.Statement (Statement)
-import qualified Hasql.Transaction as HT
-import qualified Hasql.Transaction.Sessions as HT
+import Hasql.Transaction qualified as HT
+import Hasql.Transaction.Sessions qualified as HT
 
 --------------------------------------------------------------------------------
 
@@ -37,6 +39,9 @@ instance (Has HSQL.Pool env, MonadIO m) => MonadDB (ReaderT env m) where
     pool <- Reader.asks Has.getter
     liftIO $ HSQL.Pool.use pool (HT.transaction HT.Serializable HT.Write tx)
 
+instance (MonadDB m) => MonadDB (ExceptT e m) where
+  runDB = lift . runDB
+  runDBTransaction = lift . runDBTransaction
 
 execStatement :: (MonadDB m) => Statement () a -> m (Either UsageError a)
 execStatement = runDB . Hasql.statement ()
