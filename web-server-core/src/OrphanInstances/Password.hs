88415b22befca5f0be0522b5ef20592c6bbce1d5
solomon <ssbothwell@gmail.com>
2026-02-24 19:50:07 -0800

    fix: Don't display or serialize passwords.

--- web-server-core/src/OrphanInstances/Password.hs
+++ web-server-core/src/OrphanInstances/Password.hs
@@ -9,20 +9,19 @@ import Data.Aeson qualified as Aeson
 import Data.Password.Argon2 (Password, PasswordHash (..), mkPassword)
 import Data.Password.Validate (CharacterCategory (..), InvalidReason (..))
 import Data.Text (Text)
-import Data.Text qualified as Text
 import Data.Text.Display (Display (..), display)
 import Hasql.Interpolate (DecodeValue, EncodeValue)
 
 --------------------------------------------------------------------------------
 
 instance Aeson.ToJSON Password where
-  toJSON pass = Aeson.String (Text.pack $ show pass)
+  toJSON _ = Aeson.String "<PASSWORD>"
 
 instance Aeson.FromJSON Password where
   parseJSON = Aeson.withText "Password" (pure . mkPassword)
 
 instance Display Password where
-  displayBuilder = displayBuilder . show
+  displayBuilder _ = displayBuilder @Text "<PASSWORD>"
 
 instance Display (PasswordHash x) where
   displayBuilder _ = displayBuilder @Text "<PASSWORD_HASH>"
